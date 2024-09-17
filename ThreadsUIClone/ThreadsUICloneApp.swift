4a5bbe883651b1503353d1854abc083406657f42
epdsant <epdsant@gmail.com>
2024-09-17 17:14:58 +0100

    Initial Commit

---
+++ ThreadsUIClone/ThreadsUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsUICloneApp.swift
+//  ThreadsUIClone
+//
+//  Created by EDSON SANTOS on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
